dcb53689f62ccb2cae78c98d061e6c7acdb51305
6ixFeetWinter <104249266+6ixFeetWinter@users.noreply.github.com>
2022-12-05 09:52:35 +0900

    new

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,5 @@
 document.querySelector('.menuWapper').addEventListener('click',()=>{
     document.querySelector('.menuline').classList.toggle('active');
+    document.querySelector('.pc').classList.toggle('menuActive');
+    document.querySelector('.menuText').classList.toggle('ternColor');
 });
