066a980f5ca125f2205ae81ddb5517edfca5ef63
6ixFeetWinter <k020g1278@g.neec.ac.jp>
2023-01-18 00:00:26 +0900

    nenene

---
+++ asset/asset.js
@@ -0,0 +1,14 @@
+
+let winH = window.innerHeight;
+const target = document.querySelector(".last");
+let targetpos = target.getBoundingClientRect().top + window.pageYOffset;
+window.addEventListener('resize',function(){
+    winH = window.innerHeight;
+    targetpos = target.getBoundingClientRect().top + window.pageYOffset;
+});
+window.addEventListener('scroll',function(){
+    let scroll = window.scrollY;
+    if(scroll > targetpos){
+        window.localStorage.setItem(page,1);
+    }
+});
